d964af69672e98c4ff6a5d9a9bde9a6b5aab2c46
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-08-08 13:16:04 +0600

    added: error page responsive

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,19 +1,19 @@
-import Image from 'next/image'
-import React from 'react'
+import React from 'react';
 
 export default function NotFoundPage404() {
-    return (
-        <div className="min-h-screen flex flex-col items-center justify-center text-center p-4">
-            <div className="w-full max-w-3xl">
-                <Image
-                    src="/img/error/not-found.svg"
-                    alt="error"
-                    width={1440}
-                    height={600}
-                    className="w-full h-auto"
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center p-4 sm:p-8 md:p-12 mt-12">
+      <div
+        className="
+          w-full max-w-4xl 
+          aspect-[4/3] sm:aspect-[5/3] 
+          bg-center bg-cover bg-no-repeat 
+          bg-[url('/img/error/not-found.svg')]
+        "
+      ></div>
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold mt-8 text-gray-700">
+        Oops! Page not found.
+      </h1>
+    </div>
+  );
 }
-
