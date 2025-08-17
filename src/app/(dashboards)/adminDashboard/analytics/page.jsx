6f2708fa2a7fbed2f34ac55c3069db326ec1c62c
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-08-17 21:59:28 +0600

    added: admin layout fix

--- src/app/(dashboards)/adminDashboard/analytics/page.jsx
+++ src/app/(dashboards)/adminDashboard/analytics/page.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className="pt-32 lg:pt-24">
+        <h1 className="text-3xl font-bold mb-8 text-center">Analytics</h1>
+    </div>
   )
 }
--- src/app/(dashboards)/adminDashboard/manageEvents/page.jsx
+++ src/app/(dashboards)/adminDashboard/manageEvents/page.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function ManageEvents() {
   return (
-    <div>ManageEvents</div>
+    <div className="pt-32 lg:pt-24">
+        <h1 className="text-3xl font-bold mb-8 text-center">Manage Events</h1>
+    </div>
   )
 }
