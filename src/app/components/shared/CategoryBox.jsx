6a7ab64d33a1156c4ee6e89be6be72600778e502
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-08-22 14:25:07 +0600

    remove: recent news

--- src/app/components/shared/CategoryBox.jsx
+++ src/app/components/shared/CategoryBox.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 
 export default function CategoryBox({ category }) {
-    const { id, name, bgColor, img } = category;
+    const { name, bgColor, img } = category;
     return (
         <div className="relative w-full h-60 rounded overflow-hidden hover:scale-105 transition duration-300 cursor-pointer">
             {/* Background image */}
--- src/app/components/shared/NewsCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NewsCard() {
-  return (
-    <div>
-        this is news card
-    </div>
-  )
-}
