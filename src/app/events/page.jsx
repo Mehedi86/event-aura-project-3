774b30ebc1abaa8ea4c650437fa5dcd2340908dd
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-01-17 17:41:03 +0600

    added: count

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -8,13 +8,16 @@ export default async function Events() {
     const events = await eventCollection.find({}).toArray();
     console.log(events)
 
-    
+
     return (
-        <div className="w-11/12 lg:w-4/5 mx-auto px-6 py-48 grid md:grid-cols-3 gap-6">
-            {events.length === 0 && <p>No events found.</p>}
-            {events.map((event) => (
-                <EventCard key={event._id} event={event} />
-            ))}
+        <div className="py-48">
+            <p className="text-6xl text-center">{events.length}</p>
+            <div className="w-11/12 lg:w-4/5 mx-auto px-6  grid md:grid-cols-3 gap-6">
+                {events.length === 0 && <p>No events found.</p>}
+                {events.map((event) => (
+                    <EventCard key={event._id} event={event} />
+                ))}
+            </div>
         </div>
     );
 }
